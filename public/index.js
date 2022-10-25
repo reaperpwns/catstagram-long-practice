565c906979ff4a1fc6d4fe8fb19e7e1fb2d4d6d4
royjohnlee <royjohnlee@DESKTOP-BO5FP47.localdomain>
2022-10-25 15:11:12 -0700

    working on img

--- public/index.js
+++ public/index.js
@@ -1,4 +1,29 @@
 window.onload = async () => {
-    const pic = await fetch('https://api.thecatapi.com/v1/images/search');
-    
+    const res = await fetch('https://api.thecatapi.com/v1/images/search');
+    const data = await res.json()
+    console.log("res: ", res)
+    console.log("data: ", data)
+
+
+    const url = data[0].url
+    console.log("url: ", url)
+
+    // const urlParts = url.split("/")
+    // console.log("urlParts: ", urlParts)
+    // const randomCat = urlParts[4]
+    // console.log("randomCat: ", randomCat)
+
+    const img = document.createElement("img");
+
+    console.log(img)
+
+    img.setAttribute("src", url)
+    // img.id = "cat-image"
+
+
+
+
+
+
+
 }
