6e3cf16b0661ab7186b01de7c85c5f2538c5c571
nick <nickadaniya@gmail.com>
2022-10-26 11:44:19 -1000

    event listeners

--- public/index.js
+++ public/index.js
@@ -1,62 +1,15 @@
-import { createMainContent } from "./main";
+import { createMainContent } from "./main.js";
+import { initializePage } from "./main.js";
+import { interaction } from "./main.js";
 
 
 window.onload = async () => {
-    // initializePage()
-    // createMainContent()
-
-    const res = await fetch('https://api.thecatapi.com/v1/images/search');
-    const data = await res.json()
-    // console.log("res: ", res)
-    // console.log("data: ", data)
-
-
-    const url = data[0].url
-    // console.log("url: ", url)
-
-    // const urlParts = url.split("/")
-    // console.log("urlParts: ", urlParts)
-    // const randomCat = urlParts[4]
-    // console.log("randomCat: ", randomCat)
-
-    const div = document.createElement("div")
-    const img = document.createElement("img");
-
-    console.log(img)
-
-    img.setAttribute("src", url)
-    // img.id = "cat-image"
-
-    document.body.append(div)
-    div.append(img)
-    img.style.width = "400px" // hard coded
-    const buttons = document.createElement("buttons");
-    const button1 = document.createElement("button");
-    const button2 = document.createElement("button");
-    const button3 = document.createElement("button");
-    const input = document.createElement("input");
-
-
-
-    div.append(buttons)
-    buttons.append(button1)
-    buttons.append(button2)
-    buttons.append(button3)
-    button1.id = "vote-pic"
-    button2.id = "delete-comment"
-    button3.id = "new-kitten"
-    button1.innerText = "vote"
-    button2.innerText = "delete comment"
-    button3.innerText = "random cat"
-    button1.type = "button"
-    button2.type = "button"
-    button3.type = "button"
-
-
-
-    div.append(input);
-    input.type = "text"
-    input.id = "comment"
-
+    initializePage()
+    createMainContent()
+    interaction();
     console.log(document.body)
 }
+
+window.addEventListener("click", () => {
+    
+})
--- public/main.js
+++ public/main.js
@@ -1,11 +1,53 @@
-const intializePage = () => {
-
+export const initializePage = () => {
+    const mainContainer = document.createElement('section');
+    mainContainer.className = "designContainer";
+    mainContainer.style.display = "flex";
+    mainContainer.style.width = "500px";
+    mainContainer.style.justifyContent = "center"
+    mainContainer.style.alignItems = "center"
+    mainContainer.style.flexDirection = "column";
+    document.body.appendChild(mainContainer);
 }
 
 
-
 export const createMainContent = () => {
-    const h1 = document.createElement("h1")
-    console.log(h1)
-    h1.innerText
+    const h1 = document.createElement("h1");
+    h1.innerText = "Catstagram"
+    const container = document.querySelector(".designContainer");
+    container.appendChild(h1)
+    const img = document.createElement('img');
+    img.style.width = "100px";
+    container.appendChild(img);
+    fetchImg();
+}
+
+const fetchImg = async () => {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?size=small")
+    const data = await response.json();
+    const catImg = document.querySelector('img');
+    catImg.src = data[0].url;
+}
+
+export const interaction = () => {
+    const button1 = document.createElement('button');
+    const button2 = document.createElement('button');
+    const button3 = document.createElement('button');
+    const button2Input = document.createElement('input');
+    button1.id = "vote-pic";
+    button1.innerText = "vote"
+    button1.type = "button"
+    button2.id = "comment";
+    button2.type = "button"
+    button2.innerText = "comment"
+    button2Input.id = "comment-input";
+    button2Input.type = "input";
+    button2Input.placeholder = "Say something nice!";
+    button3.id = "random-cat";
+    button3.type = "button";
+    button3.innerText = "random cat!"
+    const container = document.querySelector('.designContainer');
+    container.appendChild(button1)
+    container.appendChild(button2)
+    container.appendChild(button2Input)
+    container.appendChild(button3)
 }
